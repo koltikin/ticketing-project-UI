7d7f42827f40cc747de6615c44f9b7ce30cf0b3b
ziya <koltikin210@gmail.com>
2023-08-21 18:25:42 +0300

    user create page is done

--- assets/libs/js/dashboard-sales.js
+++ assets/libs/js/dashboard-sales.js
@@ -47,24 +47,6 @@ $(function() {
                 }
             }]
         },
-
-
-         scales: {
-                                    xAxes: [{
-                                        ticks: {
-                                            fontSize: 14,
-                                            fontFamily: 'Circular Std Book',
-                                            fontColor: '#71748d',
-                                        }
-                                    }],
-                                    yAxes: [{
-                                        ticks: {
-                                            fontSize: 14,
-                                            fontFamily: 'Circular Std Book',
-                                            fontColor: '#71748d',
-                                        }
-                                    }]
-                                }
         
                 }
             });
